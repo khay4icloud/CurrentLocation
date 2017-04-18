f920f920e9065475f453324a8b1a76a16ea8677f
Sri Kalyan Ganja <Kal_Yan@sris-macbook-pro.local>
2017-04-18 12:38:21 -0500

    Initial Commit

---
+++ CurrentLoaction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrentLoaction
+//
+//  Created by Sri Kalyan Ganja on 4/18/17.
+//  Copyright © 2017 KLYN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
